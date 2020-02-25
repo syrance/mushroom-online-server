390e47994d2874733f855560aa7c16dcf071d814
syrance <58377869+syrance@users.noreply.github.com>
2020-02-25 14:41:58 +0800

    first commit

---
+++ pages/goods/index.js
@@ -0,0 +1,73 @@
+// pages/goods/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    arr: [{
+      name: 'zhang',
+      id: '4'
+    }, {
+      name: '你好',
+      id: '5'
+    }],
+    imgUrl: 'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=342103755,377541958&fm=111&gp=0.jpg'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
